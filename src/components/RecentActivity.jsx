19cce7dd6b6626e32050d9ae033673778a8ee1b6
Ahmad Muhammad <theahmadm3@gmail.com>
2024-05-31 22:19:31 +0100

    added recent activity pictures

--- src/components/RecentActivity.jsx
+++ src/components/RecentActivity.jsx
@@ -1,14 +1,32 @@
-
+import { activities } from "../data/activities";
 
 function RecentActivity() {
     return (
-        <section className="w-100 pa2 flex flex-column justify-between" id="activity">
-            <h1 className="w-100 tc">Recent Activity</h1>
-            <div className="w-100 pa7 inline-flex justify-around">
-                
+        <section className="pa2 flex flex-column justify-between" id="activity">
+            <h1 className="w-100 tc">Recent Activities</h1>
+            <div className="mt2 shadow-1 pa1 br3 mb2 pb2 overflow-auto flex flex-wrap justify-between" style={{ minHeight: '400px' }}>
+                {activities.map((research) => (
+                    <div key={research.id} className="activity-card flex-shrink-0 br3 pa2 ma2">
+                        <div
+                            style={{
+                                backgroundImage: `url(${research.image})`,
+                                height: '300px',
+                                backgroundSize: 'contain',
+                                backgroundPosition: 'center',
+                                backgroundRepeat: 'no-repeat',
+                                borderRadius: '8px'
+                            }}
+                            className="activity-image"
+                        ></div>
+                        <div className="activity-details bg-dark-gray pa3 br3 mt2">
+                            <p className="activity-date white-60">{research.date}</p>
+                            <h2 className="activity-title white">{research.title}</h2>
+                        </div>
+                    </div>
+                ))}
             </div>
         </section>
-    )
+    );
 }
 
-export default RecentActivity
+export default RecentActivity;
---
+++ src/data/activities.js
@@ -0,0 +1,17 @@
+import activity1 from '../assets/images/activity1.jpeg'
+import activity2 from '../assets/images/activity2.jpeg'
+
+export const activities = [
+    {
+        id: 1,
+        image: activity1,
+        title: 'Activity Title 1',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel urna a orci cursus.',
+    },
+    {
+        id: 2,
+        image: activity2,
+        title: 'Activity Title 2',
+        description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    },
+];
