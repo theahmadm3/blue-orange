562dc4dd922b67f367a23cceb0b23e87ebfb9c51
Ahmad Muhammad <theahmadm3@gmail.com>
2024-06-24 00:09:16 +0100

    chore: Update student data with Ibrahim Aliyu's information and research details

--- src/data/students.js
+++ src/data/students.js
@@ -1,18 +1,25 @@
 import koloImage from '../assets/images/umarkolo.jpeg'
+import ibraAliImage from '../assets/images/IbroAli.jpg'
 
 export const students = [
     {
         id: 1,
-        name: 'Ibrahim',
-        email: '20233539@nileuniversity.edu.ng',
-        avatar: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-2.png',
-        topic: 'No data yet',
+        name: 'Ibrahim Aliyu',
+        email: '20232903@nileuniversity.edu.ng',
+        avatar: ibraAliImage,
+        topic: 'OPTIMIZATION OF BASE SEAL AND RAP IN THE STABILIZATION OF WEAK SOIL USING SURFACE RESPONSE METHODOLOGY (SRM)',
         supervisor: 'No data yet',
         supervisor2: 'No data yet',
         abstract: 'No data yet',
-        introduction: 'No data yet',
-        methodology: 'No data yet',
-        findings: 'No data yet',
+        introduction: `The depletion of natural materials necessitates the use of recycled materials, particularly from construction and demolition waste, which reduces pollution due to hazardous chemicals in these wastes. (Sanei & Modarres, 2023).
+        Untreated clays in highway construction can hinder compaction, leading to low bearing capacity, unacceptable settlements, and slope stability under traffic loading.(Du et al., 2016)
+        Natural aggregate extraction in road pavement construction generates significant environmental impacts, prompting studies on alternative materials for construction and demolition to reduce these impacts. (Bastidas-Martínez et al., 2022).
+        Road construction is a critical industry due to the growing importance of transportation and heavy-duty machinery, necessitating technical, economic, and environmental studies for its construction and maintenance. (Sanei & Modarres, 2023).
+        Daniel et al., (2017) Base material stabilization for weak soils is a permanent improvement in the base aggregate layer, enhancing elastic and strength characteristics, and can be classified as chemical or bitumen stabilization.
+        Base-Seal International Inc., a global leader in soil stabilization, collaborates with CMS Engineering Group to improve soil bearing capacity, reduce plasticity index, and reduce permeability, using Liquid Soil Stabilizer and Liquid Polymer Sealant.
+        `,
+        methodology: 'Materials The materials used for this research include; a. Reclaimed Asphalt Pavement Material. b. Laterite (subbase material) c. Cement d. Liquid stabilizer (Base seals) This study covers; Location: Abuja F.C.T (North-Central Nigeria Methods 1. Material Characterization: RAP Analysis, Base Seal Properties: 2. Soil Testing: Soil classification, Mechanical testing 3. Mix Design and Optimization: mix proportioning, optimization trials 4. Performance Evaluation: Strength and stability test, Permeability testing 5. Durability Assessment: Freez – Thaw testing, Wet-Dry cycling 6. Microstructural Analysis: Scanning Microscopy (SEM).',
+        findings: 'Expected Results In summary, this study aimed to enhance the strength properties of a weak soil using chemical additive (BS) and recycled asphalt pavement (RAP) based on the Response Surface Method (RSM) approach. Microstructural studies will also be carried out to understand how the soil’s properties were affected by adding these materials. It is expected that, the optimum content of BS/RAP in the stabilization soil that will Maximum Dry Density (MDD), Unconfined Compressive Strength (UCS), CBR, will be determined. The analyses will reveal the most influential factor that affect the strength properties. Additionally, SEM analyses will be carried out to assess the compositional changes resulting from the soil’s interaction with the base seal and RAP in understanding the mechanisms responsible for improving the treated soil’s mechanical behavior.',
         conclusion: 'No data yet',
         publications: [
             'No data yet',
@@ -23,8 +30,7 @@ export const students = [
             'No data yet'
         ],
         publicationLink: [
-            { name: 'No data yet', url: 'No data yet' },
-            { name: 'No data yet', url: 'No data yet' }
+            'No data yet'
         ],
     },
     {
